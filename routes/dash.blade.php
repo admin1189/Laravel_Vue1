606e69789d52cc63b9d55202fc35f797adfd0e6b
admin1189 <Mydrambluesky118@outlook.com>
2020-01-29 04:31:45 +1000

    create project

---
+++ routes/dash.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard - Tutsmake.com</title>
+ 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<!--Bootsrap 4 CDN-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ 
+<link rel="stylesheet" type="text/css" href="{{url('style.css')}}">
+ 
+</head>
+<body>
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">Welcome Dashboard! hi</h3>
+              <div class="card">
+                  <div class="card-body">
+                    Welcome 
+                  </div>
+                  <div class="card-body">
+                    <a class="small" href="{{url('logout')}}">Logout</a>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">User list</h3>
+               
+               @foreach ($users as $user)
+                <div class="form-label-group">
+                  <input type="email" name="email" id="inputEmail" class="form-control" placeholder="{{$user->email}}" >
+
+                  </div> 
+               @endforeach
+ 
+                
+            </div>
+
+    </div>
+    <div class="container">
+        <div id="app"></div>
+    </div>
+    <script src="/js/app.js"></script>
+</div>
+ 
+</body>
+</html>
